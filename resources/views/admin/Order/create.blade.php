2664ce1dc77dca137fe6dc6321e19f8af9e74f68
Avinash Antala <antalaavi@gmail.com>
2020-10-30 15:29:48 +0530

    Avinash : Added imagemaps in order form Added datepicker and some other in order form changes composer require yajra/laravel-datatables-oracle

--- resources/views/admin/Order/create.blade.php
+++ resources/views/admin/Order/create.blade.php
@@ -8,13 +8,27 @@
 <h1>Create Job Order</h1>
 @stop
 
+@section('css')
+    <link rel="stylesheet" href="{{asset('vendor/datepicker/datepicker3.css') }}">
+    <style>
+        .car_image_div {
+            width: 100%;
+            text-align: center;
+        }
+        img[usemap] {
+            border: none;
+            height: auto;
+            max-width: 100%;
+            width: auto;
+        }
+        input[type=checkbox]{
+            height:22px;
+            width:22px;
+        } 
+    </style>
+@stop
+
 @section('content')
-<style>
-    input[type=checkbox]{
-        height:22px;
-        width:22px;
-    } 
-</style>
 <div class="card">
     <div class="card-body">
         @if ($errors->any())
@@ -40,17 +54,17 @@
             <div class="col-md-3">
                 <div class="form-group">
                     {{ Form::label('date', 'Date', ['class' => 'control-label']) }} <span class="text-danger">*</span>
-                    {{ Form::date('date', $item_details_data->date ?? '', ['id' => '', 'class' => 'form-control datepicker']) }}
+                    {{ Form::text('date', $item_details_data->date ?? date('d-m-Y'), ['id' => '', 'class' => 'form-control datepicker']) }}
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="form-group">
                     {{ Form::label('time', 'Time', ['class' => 'control-label']) }} <span class="text-danger">*</span>
-                    {{ Form::time('time', $item_details_data->date ?? '', ['id' => '', 'class' => 'form-control datepicker']) }}
+                    {{ Form::time('time', $item_details_data->time ?? date('H:i A'), ['id' => '', 'class' => 'form-control']) }}
                 </div>
             </div>
-            <div class="col-md-3"></div>
-            <div class="clearfix"></div><br/>
+        </div>
+        <div class="row">
             <div class="col-md-3">
                 <div class="form-group">
                     {{ Form::label('mobile', 'Mobile', ['class' => 'control-label']) }} <span class="text-danger">*</span>
@@ -63,10 +77,8 @@
                     {{ Form::text('receiver_name', $item_details_data->receiver_name ?? '', ['id' => 'receiver_name', 'class' => 'form-control']) }}
                 </div>
             </div>
-            <div class="clearfix"></div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
-            <div class="clearfix"></div><br/>
+        </div>
+        <div class="row">
             <div class="col-md-3">
                 <div class="form-group">
                     {{ Form::label('carid', 'Car', ['class' => 'control-label']) }} <span class="text-danger">*</span>
@@ -93,37 +105,53 @@
             </div>
             <div class="col-md-3">
                 <div class="form-group">
-                    {{ Form::label('model_year', 'Model Year', ['class' => 'control-label']) }} <span class="text-danger">*</span>
+                    {{ Form::label('model_year', 'Model Year', ['class' => 'control-label']) }}
                     {{ Form::text('model_year', $item_details_data->model_year ?? '', ['id' => 'model_year', 'class' => 'form-control']) }}
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="form-group">
-                    {{ Form::label('mileage', 'Mileage', ['class' => 'control-label']) }} <span class="text-danger">*</span>
+                    {{ Form::label('mileage', 'Mileage', ['class' => 'control-label']) }}
                     {{ Form::text('mileage', $item_details_data->mileage ?? '', ['class' => 'form-control']) }}
                 </div>
             </div>
+        </div>
+        <div class="row">
             <div class="col-md-3">
                 <div class="form-group">
                     {{ Form::label('ex_delivery_date', 'Expected Date of Delivery', ['class' => 'control-label']) }} <span class="text-danger">*</span>
-                    {{ Form::text('ex_delivery_date', $item_details_data->ex_delivery_date ?? '', ['id' => 'ex_delivery_date', 'class' => 'form-control']) }}
+                    {{ Form::text('ex_delivery_date', $item_details_data->ex_delivery_date ?? '', ['id' => 'ex_delivery_date', 'class' => 'form-control datepicker']) }}
                 </div>
             </div>
-            
             <div class="col-md-3">
                 <div class="form-group">
-                    {{ Form::label('photo', 'Photo', ['class' => 'control-label']) }} <span class="text-danger">*</span>
+                    {{ Form::label('price', 'Price', ['class' => 'control-label']) }} <span class="text-danger">*</span>
+                    {{ Form::text('price', $item_details_data->price ?? '', ['class' => 'form-control num_only']) }}
+                </div>
+            </div>
+        </div>
+        <div class="car_image_div">
+            <img src="{{asset('images/car-tap-4.jpg')}}" id="img_1" style="height:400px;" usemap="#car_job_parts" alt="" />
+            <map name="car_job_parts">
+                <area alt="Left First Wheel" title="Left First Wheel" href="#LeftFirstWheel" coords="45,206,38" shape="circle">
+                <area alt="Car Bonate" title="Car Bonate" href="#CarBonate" coords="165,132,161,168,158,207,157,231,155,249,168,241,181,236,193,230,206,228,220,226,238,225,252,225,267,226,279,226,292,227,301,228,313,229,323,232,335,237,347,241,362,246,367,251,368,243,367,231,366,219,366,206,366,193,366,180,364,163,363,153,363,142,359,128,340,121,319,119,293,115,268,115,242,117,224,116,204,117,191,120,179,124" shape="poly">
+                <area alt="Right First Wheel" title="Right First Wheel" href="#RightFirstWheel" coords="479,206,38" shape="circle">
+            </map>
+        </div>
+        {{ Form::button('Save', ['class' => 'btn btn-primary module_save_btn disabled'], 'disabled') }}
+        <!--{{ Form::submit('Save', ['class' => 'btn btn-primary module_save_btn disabled'], 'disabled') }}-->
+        
+        {{ Form::close() }}
+        <br><br><hr>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-group">
+                    {{ Form::label('photo', 'Photo', ['class' => 'control-label']) }}
                     {{ Form::file('photo', []) }}
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="form-group">
-                    {{ Form::label('price', 'price', ['class' => 'control-label']) }} <span class="text-danger">*</span>
-                    {{ Form::text('price', $item_details_data->price ?? '', ['class' => 'form-control']) }}
-                </div>
-            </div>
-            <div class="col-md-3"></div>
-            <div class="clearfix"></div><br/>
+        </div>
+        <div class="row">
             <div class="col-md-4">
                 <div class="form-group">
                     {{ Form::checkbox('', '1', $item_details_data->item_available ?? '[checked]') }}
@@ -354,22 +382,50 @@
             </div>
             <div class="col-md-3"></div>
         </div>
-        
-        {{ Form::button('Save', ['class' => 'btn btn-primary module_save_btn disabled'], 'disabled') }}
-        <!--{{ Form::submit('Save', ['class' => 'btn btn-primary module_save_btn disabled'], 'disabled') }}-->
-        
-        {{ Form::close() }}
+    </div>
+</div>
+
+<!-- The Modal -->
+<div class="modal fade" id="AddPartTextModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form class="form-horizontal" method="post" id="add_car_part_details">
+                <input type="hidden" name="car_part_name" id="car_part_name">
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title"></h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <label for="car_part_detail">Add Detail :</label>
+                    <textarea name="car_part_detail" id="car_part_detail" class="form-control"></textarea>
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @stop
 
 @section('js')
+    <script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>
+    <script src="{{asset('vendor/datepicker/bootstrap-datepicker.js') }}"></script>
+    <script src="{{asset('js/jquery.rwdImageMaps.min.js') }}"></script>
     <script>
+        var car_part_details = {};
         $(document).ready( function(){
-            
-            
-            
-            
+            $('.datepicker').datepicker({
+                format: 'dd-mm-yyyy',
+                todayBtn: "linked",
+                autoclose: true
+            });
             $(document).on('input', ".num_only", function () {
                 var textbox_value = this.value = this.value.replace(/[^\d\.\-]/g, '');
                 var textbox_str = textbox_value.split(/\./);
@@ -378,6 +434,33 @@
                 }
             });
             
+            $('img[usemap]').rwdImageMaps();
+            $(document).on('click', 'area', function () {
+                $('#AddPartTextModal').modal('show');
+                $('.modal-title').html('Add Details for - ' + $(this).attr('alt'));
+                var car_part_name = $(this).attr('href');
+                $('#car_part_name').val(car_part_name);
+                if(car_part_details[car_part_name]){
+                    var car_part_detail = car_part_details[car_part_name];
+                    $('#car_part_detail').val(car_part_detail);
+                }
+            });
+
+            $('#AddPartTextModal').on('shown.bs.modal', function () {
+                $('#car_part_detail').focus();
+            });
+
+            $('#AddPartTextModal').on('hidden.bs.modal', function () {
+                $('#car_part_name').val('');
+                $('#car_part_detail').val('');
+            });
+            $(document).on('submit', '#add_car_part_details', function () {
+                var car_part_name = $('#car_part_name').val();
+                car_part_details[car_part_name] = $('#car_part_detail').val();
+                console.log(car_part_details);
+                $('#AddPartTextModal').modal('hide');
+                return false;
+            });
             
             $(document).on('submit', '#save_order_details', function(){
                 $('.module_save_btn').attr('disabled', 'disabled');
